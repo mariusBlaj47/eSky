909ad21def19d802ddd2276556ef6cd4bcfaf6ea
adriandlupu <48430145+adriandlupu@users.noreply.github.com>
2019-05-28 20:42:18 +0300

    ?

--- eSky MVC/eSky/app/controllers/addFlight.php
+++ eSky MVC/eSky/app/controllers/addFlight.php
@@ -2,11 +2,31 @@
 
 class addFlight extends Controller
 {
+
     public function index()
     {
         session_start();
         if(!isset($_SESSION['id']))
             header('Location: ' . URL . 'Home');
-        $this->view('addFlight/index');
+        $airlines_model = $this->loadModel('AirlinesModel');
+        $airports = $airlines_model->getAirports();
+        $data['airports']="";
+        foreach (array_values($airports) as $airport)
+        {
+            $data['airports'] = $data['airports'] . '<option>'.$airport.'</option>';
+        }
+        $this->view('addFlight/index',$data);
+    }
+    public function add()
+    {
+        print_r($_POST);
+        session_start();
+        if(!isset($_SESSION['id']))
+            header('Location: ' . URL . 'Home');
+        if(isset($_POST['origin'])&&isset($_POST['destination'])&&isset($_POST['date1'])&&isset($_POST['date2'])&&isset($_POST['hour1'])&&isset($_POST['min1'])&&isset($_POST['am1'])&&isset($_POST['hour2'])&&isset($_POST['min2'])&&isset($_POST['am2'])&&isset($_POST['price'])&&isset($_POST['ensurance'])&&isset($_POST['seat']))
+        {
+            $date1 = date('d-M-y', strtotime($_POST['date1']));
+            $date2 = date('d-M-y', strtotime($_POST['date2']));
+        }
     }
 }
--- eSky MVC/eSky/app/views/addFlight/index.php
+++ eSky MVC/eSky/app/views/addFlight/index.php
@@ -1,1 +1,196 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link type="text/css" rel="stylesheet" href="<?php echo URL; ?>public/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?php echo URL; ?>public/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<header>
+    <nav>
+        <div>
+            <h1><a href="<?php echo URL; ?>Home"><img src="<?php echo URL; ?>public/images/logo.png" alt="logo"></a></h1>
+        </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Our Services</a></li>
+            <li><a href="#">Design</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+<body>
+    <div id="booking" class="section">
+        <div class="section-center">
+            <div class="container">
+                <div class="row">
+                    <div class="booking-form">
+                        <form action="<?php echo URL; ?>addFlight/add" method="post">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">Origin</span>
+                                        <select class="form-control" name="origin" id="originSelect">
+                                            <?php echo $data['airports']; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">Destination</span>
+                                        <select class="form-control" name="destination" id="destinationSelect" >
+                                            <?php echo $data['airports']; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">Departure Date</span>
+                                        <input class="form-control" name="date1" type="date" required>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <span class="form-label">Arrival Date</span>
+                                        <input class="form-control" name="date2" type="date" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="form-label">Departure</span>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <span class="form-label">Hour</span>
+                                        <select class="form-control" name="hour1">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                            <option>6</option>
+                                            <option>7</option>
+                                            <option>8</option>
+                                            <option>9</option>
+                                            <option>10</option>
+                                            <option>11</option>
+                                            <option>12</option>
+                                        </select>
+                                        <span class="select-arrow"></span>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <span class="form-label">Min</span>
+                                        <select class="form-control" name="min1">
+                                            <option>05</option>
+                                            <option>10</option>
+                                            <option>15</option>
+                                            <option>20</option>
+                                            <option>25</option>
+                                            <option>30</option>
+                                            <option>35</option>
+                                            <option>40</option>
+                                            <option>45</option>
+                                            <option>50</option>
+                                            <option>55</option>
+                                        </select>
+                                        <span class="select-arrow"></span>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <span class="form-label">AM/PM</span>
+                                        <select class="form-control" name="am1">
+                                            <option>AM</option>
+                                            <option>PM</option>
+                                        </select>
+                                        <span class="select-arrow"></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="form-label">Arrival</span>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <span class="form-label">Hour</span>
+                                        <select class="form-control" name="hour2">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                            <option>6</option>
+                                            <option>7</option>
+                                            <option>8</option>
+                                            <option>9</option>
+                                            <option>10</option>
+                                            <option>11</option>
+                                            <option>12</option>
+                                        </select>
+                                        <span class="select-arrow"></span>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <span class="form-label">Min</span>
+                                        <select class="form-control" name="min2">
+                                            <option>05</option>
+                                            <option>10</option>
+                                            <option>15</option>
+                                            <option>20</option>
+                                            <option>25</option>
+                                            <option>30</option>
+                                            <option>35</option>
+                                            <option>40</option>
+                                            <option>45</option>
+                                            <option>50</option>
+                                            <option>55</option>
+                                        </select>
+                                        <span class="select-arrow"></span>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <span class="form-label">AM/PM</span>
+                                            <select class="form-control" name="am2">
+                                                <option>AM</option>
+                                                <option>PM</option>
+                                            </select>
+                                            <span class="select-arrow"></span>
+                                        </div>
+                                    </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <span class="form-label">Price</span>
+                                        <input class="form-control" type="number" name="price" placeholder="Choose price" required>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <span class="form-label">Ensurance price</span>
+                                        <input class="form-control" type="number" name="ensurance" placeholder="Choose price" required>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <span class="form-label">Number of seats</span>
+                                        <input class="form-control" type="number" name="seat" placeholder="How many" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-btn">
+                                <button class="submit-btn">Add Now</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
